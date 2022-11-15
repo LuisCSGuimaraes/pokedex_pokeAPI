1bdec44fe48c0d4a1448189b8e28b7752c67e5a4
Luis Guimarães <luis.guimaraes@gmail.com>
2022-11-15 02:31:04 -0300

    Promises com Arrow Function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,8 @@
 const offset = 0
-const limit = 10 
+const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
-.catch(function(error){
-    console.error(error)
-})
-.finally(function(){
-    console.log('Requisição concluída')
-})
+fetch(url) //Utilizando Arrow Function
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
