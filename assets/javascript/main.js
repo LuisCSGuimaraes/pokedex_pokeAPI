06d866b2f5e001f52f56833e452269c0c2a34442
Luis Guimarães <luis.guimaraes@gmail.com>
2022-11-17 02:39:00 -0300

    Término da tela base

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,29 +1,49 @@
-//Função para converter o response em uma li do html
-function convertPokemonToHtml(pokemon) {
-    return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">   
-                <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
-                </ol>
-
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-            </div>
-        </li>
-
-    `
-}
-
 //Capurar a ol do html
 const pokemonList = document.getElementById("pokemonList")
+const loadMoreButton = document.getElementById("loadMoreButton")
+const maxRecord = 151
+const limit = 10git 
+let offset = 0
 
 /*Mapear o resultado json da requisição da PokeAPI, enviar para a função 
 convertPokemonToHtml e dicionar ao HTML original as li concatenadas*/
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
-})
+function loadPokemonItems(offset, limit){
 
+    //Função para converter o response em uma li do html
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>
+            `
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+    
+                    <div class="detail">   
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}<li>`).join('')}
+                        </ol>
+    
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </li>
+    
+            `
+        ).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItems(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtRecordNextPage = offset + limit
+
+    if (qtRecordNextPage >= maxRecord) {
+        const newLimit = maxRecord - offset
+        loadPokemonItems(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItems(offset, limit)
+    }    
+})
--- assets/javascript/poke-api.js
+++ assets/javascript/poke-api.js
@@ -1,11 +1,31 @@
 const pokeApi = {}
 
+//Função para converter o modelo dos pokemons de origem para um modelo simplificado
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+    
+} 
+
 //Função para fazer a segunda requisição na url de resposta 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url).then((response) => response.json())    
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)    
 }
 //Função para comunicar com a PokeAPI
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
     //Converter o resultado da requisição em um json
