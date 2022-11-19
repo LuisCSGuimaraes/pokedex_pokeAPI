de41e3b6c5109599dd1b5af3c4a7bb1899be68dc
Luis Guimarães <luis.guimaraes@gmail.com>
2022-11-19 03:11:57 -0300

    Configurando a tela de detalhes dos Pokemons. Inserida pokebola animada.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,18 +13,20 @@ function loadPokemonItems(offset, limit){
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) =>
             `
-                <li class="pokemon ${pokemon.type}">
-                    <span class="number">#${pokemon.number}</span>
-                    <span class="name">${pokemon.name}</span>
-    
-                    <div class="detail">   
-                        <ol class="types">
-                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}<li>`).join('')}
-                        </ol>
-    
-                        <img src="${pokemon.photo}" alt="${pokemon.name}">
-                    </div>
-                </li>
+                <a href="info.html?pokeId=${pokemon.number}">
+                    <li class="pokemon ${pokemon.type}">
+                        <span class="number">#${pokemon.number}</span>
+                        <span class="name">${pokemon.name}</span>
+        
+                        <div class="detail">   
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}<li>`).join('')}
+                            </ol>
+        
+                            <img src="${pokemon.photo}" alt="${pokemon.name}">
+                        </div>
+                    </li>
+                </a>    
     
             `
         ).join('')
